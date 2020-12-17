3306b8736fdc2f5148990abeb1a86cc6d8c56597
David Merrick <david@david-merrick.com>
2020-12-16 21:38:04 -0800

    Day 11, part 2

--- src/main/kotlin/io/github/davidmerrick/aoc2020/day11/SeatGrid.kt
+++ src/main/kotlin/io/github/davidmerrick/aoc2020/day11/SeatGrid.kt
@@ -3,30 +3,75 @@ package io.github.davidmerrick.aoc2020.day11
 class SeatGrid(private val _input: List<List<SeatType>>) {
 
     val width: Int
-    get() = _input[0].size
+        get() = _input[0].size
+
+    val height: Int
+        get() = _input.size
 
     val seats: List<List<SeatType>>
-    get() = _input.toList()
+        get() = _input.toList()
 
     fun getSeatAt(x: Int, y: Int): SeatType {
         return _input[y][x]
     }
 
-    fun getAdjacentSeats(x: Int, y: Int): List<SeatType> {
-        val xRange = when(x){
-            0 -> (0..1)
-            _input[0].size - 1 -> (x-1..x)
-            else -> (x-1..x+1)
+    fun getVisibleSeats(x: Int, y: Int): List<SeatType> {
+        return listOf(
+            // Right
+            getVisibleSeatsInDirection(x, y, 1, 0),
+            // Left
+            getVisibleSeatsInDirection(x, y, -1, 0),
+            // Up
+            getVisibleSeatsInDirection(x, y, 0, 1),
+            // Down
+            getVisibleSeatsInDirection(x, y, 0, -1),
+            // Upper left
+            getVisibleSeatsInDirection(x, y, -1, 1),
+            // Upper right
+            getVisibleSeatsInDirection(x, y, 1, 1),
+            // Lower left
+            getVisibleSeatsInDirection(x, y, -1, -1),
+            // Lower right
+            getVisibleSeatsInDirection(x, y, 1, -1)
+        ).mapNotNull { it }
+    }
+
+    private fun getVisibleSeatsInDirection(
+        x: Int,
+        y: Int,
+        deltaX: Int,
+        deltaY: Int
+    ): SeatType? {
+        var currentX = x + deltaX
+        var currentY = y + deltaY
+
+        while (currentX in (0 until width) && currentY in (0 until height)) {
+            val seat = getSeatAt(currentX, currentY)
+            if (seat != SeatType.FLOOR) {
+                return seat
+            }
+            currentX += deltaX
+            currentY += deltaY
         }
-        val yRange = when(y){
+
+        return null
+    }
+
+    fun getAdjacentSeats(x: Int, y: Int): List<SeatType> {
+        val xRange = when (x) {
             0 -> (0..1)
-            _input.size - 1 -> (y-1..y)
-            else -> (y-1..y+1)
+            _input[0].size - 1 -> (x - 1..x)
+            else -> (x - 1..x + 1)
+        }
+        val yRange = when (y) {
+            0 -> (0..1)
+            _input.size - 1 -> (y - 1..y)
+            else -> (y - 1..y + 1)
         }
 
         val adjacentSeats = mutableListOf<SeatType>()
-        for(i in xRange){
-            for (j in yRange){
+        for (i in xRange) {
+            for (j in yRange) {
                 adjacentSeats.add(getSeatAt(i, j))
             }
         }
@@ -36,9 +81,9 @@ class SeatGrid(private val _input: List<List<SeatType>>) {
 
     fun applyRules(rules: (grid: SeatGrid, x: Int, y: Int) -> SeatType): SeatGrid {
         val newInput = mutableListOf<List<SeatType>>()
-        for(j in _input.indices){
+        for (j in _input.indices) {
             val row = mutableListOf<SeatType>()
-            for (i in _input[0].indices){
+            for (i in _input[0].indices) {
                 row.add(rules(this, i, j))
             }
             newInput.add(row)
--- src/test/kotlin/io/github/davidmerrick/aoc2020/day11/Day11Test.kt
+++ src/test/kotlin/io/github/davidmerrick/aoc2020/day11/Day11Test.kt
@@ -1,6 +1,7 @@
 package io.github.davidmerrick.aoc2020.day11
 
 import io.github.davidmerrick.aoc2020.day11.SeatType.EMPTY
+import io.github.davidmerrick.aoc2020.day11.SeatType.OCCUPIED
 import io.github.davidmerrick.aoc2020.testutil.TestUtil
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldNotBe
@@ -26,9 +27,36 @@ class Day11Test {
 
     @Test
     fun `Get adjacent seats at right corner`(){
-        val expected = listOf(EMPTY, EMPTY, EMPTY)
+        val expected = listOf(OCCUPIED, EMPTY, EMPTY)
         val grid = parseSeats("day11.txt")
         val actual = grid.getAdjacentSeats(grid.width-1, 0)
+        actual.size shouldBe expected.size
+        actual.containsAll(expected)
+    }
+
+    @Test
+    fun `Get visible seats from part 2 example`(){
+        val expected = listOf(
+            OCCUPIED,
+            OCCUPIED,
+            OCCUPIED,
+            OCCUPIED,
+            OCCUPIED,
+            OCCUPIED,
+            OCCUPIED,
+            OCCUPIED
+        )
+        val grid = parseSeats("day11-part2.txt")
+        val actual = grid.getVisibleSeats(3, 4)
+        actual.size shouldBe expected.size
+        actual.containsAll(expected)
+    }
+
+    @Test
+    fun `Get visible seats from part 2 example 2`(){
+        val expected = listOf(EMPTY)
+        val grid = parseSeats("day11-part2-2.txt")
+        val actual = grid.getVisibleSeats(1, 1)
         actual.size shouldBe expected.size
         actual.containsAll(expected)
     }
@@ -58,11 +86,11 @@ class Day11Test {
             val seat = grid.getSeatAt(x, y)
             val adjacentSeats = grid.getAdjacentSeats(x, y)
             return if(seat == EMPTY &&
-                adjacentSeats.none { it == SeatType.OCCUPIED }
+                adjacentSeats.none { it == OCCUPIED }
             ){
-                SeatType.OCCUPIED
+                OCCUPIED
             } else if (
-                seat == SeatType.OCCUPIED && (adjacentSeats.count { it == SeatType.OCCUPIED } >= 4)
+                seat == OCCUPIED && (adjacentSeats.count { it == OCCUPIED } >= 4)
             ){
                 EMPTY
             } else {
@@ -81,8 +109,62 @@ class Day11Test {
         iterationCount shouldBe 6
         val occupiedCount = currentGrid.seats.asSequence()
             .flatten()
-            .count { it == SeatType.OCCUPIED }
+            .count { it == OCCUPIED }
         occupiedCount shouldBe 37
+    }
+
+    @Test
+    fun `Count occupied seats in part 2 example`(){
+        var currentGrid = parseSeats("day11.txt")
+
+        var stable = false
+        var iterationCount = 0
+        while(!stable){
+            val newGrid = currentGrid.applyRules(part2Rules)
+            stable = newGrid.seats == currentGrid.seats
+            currentGrid = newGrid
+            iterationCount++
+        }
+
+        val occupiedCount = currentGrid.seats.asSequence()
+            .flatten()
+            .count { it == OCCUPIED }
+        occupiedCount shouldBe 26
+    }
+
+    @Test
+    fun `Count occupied seats in part 2 full`(){
+        var currentGrid = parseSeats("day11-full.txt")
+
+        var stable = false
+        var iterationCount = 0
+        while(!stable){
+            val newGrid = currentGrid.applyRules(part2Rules)
+            stable = newGrid.seats == currentGrid.seats
+            currentGrid = newGrid
+            iterationCount++
+        }
+
+        val occupiedCount = currentGrid.seats.asSequence()
+            .flatten()
+            .count { it == OCCUPIED }
+        println(occupiedCount)
+    }
+
+    private val part2Rules = fun(grid: SeatGrid, x: Int, y: Int): SeatType {
+        val seat = grid.getSeatAt(x, y)
+        val visibleSeats = grid.getVisibleSeats(x, y)
+        return if(seat == EMPTY &&
+            visibleSeats.none { it == OCCUPIED }
+        ){
+            OCCUPIED
+        } else if (
+            seat == OCCUPIED && (visibleSeats.count { it == OCCUPIED } >= 5)
+        ){
+            EMPTY
+        } else {
+            seat
+        }
     }
 
     @Test
@@ -92,11 +174,11 @@ class Day11Test {
             val seat = grid.getSeatAt(x, y)
             val adjacentSeats = grid.getAdjacentSeats(x, y)
             return if(seat == EMPTY &&
-                adjacentSeats.none { it == SeatType.OCCUPIED }
+                adjacentSeats.none { it == OCCUPIED }
             ){
-                SeatType.OCCUPIED
+                OCCUPIED
             } else if (
-                seat == SeatType.OCCUPIED && (adjacentSeats.count { it == SeatType.OCCUPIED } >= 4)
+                seat == OCCUPIED && (adjacentSeats.count { it == OCCUPIED } >= 4)
             ){
                 EMPTY
             } else {
@@ -114,7 +196,7 @@ class Day11Test {
 
         val occupiedCount = currentGrid.seats.asSequence()
             .flatten()
-            .count { it == SeatType.OCCUPIED }
+            .count { it == OCCUPIED }
         println(occupiedCount)
     }
 
@@ -124,7 +206,7 @@ class Day11Test {
                 when(char){
                     'L' -> EMPTY
                     '.' -> SeatType.FLOOR
-                    '#' -> SeatType.OCCUPIED
+                    '#' -> OCCUPIED
                     else -> throw RuntimeException("Invalid input")
                 }
             }.toList()
